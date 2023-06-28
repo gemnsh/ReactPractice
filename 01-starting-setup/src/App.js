c68b59b704076e50d1ad416b55432404006bb2ad
gemnsh <shyeon5895@gmail.com>
2023-06-29 07:56:11 +0900

    add : gitignore & prac01

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+    return ( <div>
+        <h1> Let 's get started!!!</h1>  
+        <h2> Let 's get started!!!</h2>  
+        <h3> Let 's get started!!!</h3>  
+        <h4> Let 's get started!!!</h4>
+        <p>This is also visible</p>
+
+        </div >
+        
+    );
+}
+
+export default App;

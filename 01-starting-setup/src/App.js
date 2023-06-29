5399ea50599a90d70df8e83168dd593f584bd87e
gemnsh <shyeon5895@gmail.com>
2023-06-29 15:56:09 +0900

    edit : expense item

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -3,10 +3,6 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
     return ( <div>
-        <h1> Let 's get started!!!</h1>  
-        <h2> Let 's get started!!!</h2>  
-        <h3> Let 's get started!!!</h3>  
-        <h4> Let 's get started!!!</h4>
         <ExpenseItem></ExpenseItem>
 
         </div >

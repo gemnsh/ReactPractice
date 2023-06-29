bd35d846ffe93e3838dca1eab2cef0d05fa48e74
gemnsh <shyeon5895@gmail.com>
2023-06-29 17:41:30 +0900

    edit : date

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,10 +2,16 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props){
 
+    const month=props.date.toLocaleString('ko-KR',{month:'long'});
+    const day=props.date.toLocaleString('ko-KR',{day:'2-digit'});
+    const year=props.date.getFullYear();
     return(
         <div className="expense-item">
             <div>
-                {props.date.toISOString()}
+                {/*{props.date.toISOString()} 주석*/}
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
             </div>
             <div className="expense-item__description"> 
                 <h2>{props.title}</h2>

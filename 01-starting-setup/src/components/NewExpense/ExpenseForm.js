30943f6381e31d1fe9f2953d7bd2041f44b6f8d9
gemnsh <shyeon5895@gmail.com>
2023-06-30 20:58:59 +0900

    edit : 양방향 바인딩

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -58,24 +58,39 @@ function ExpenseForm(){
         //        enteredDate: event.target.value,
         //    }
         //)
-    ;}
+    };
+
+    const submitHandler = (event) =>{
+        event.preventDefault();     //기본 요청이 보내지는 것을 막을 수 있음
+
+        const expenseData ={
+            title : enteredTitle,
+            amount: enteredAmount,
+            date : new Date(enteredDate)
+        };
+        setEnteredTitle('');
+        console.log(expenseData);
+    };
 
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
-            <div className='new-expense_control'>
+            <div className='new-expense__control'>
                     <label>
                         Title
                     </label>
-                    <input type='text' onChange={titleChangeHandler}></input>
+                    <input
+                        type='text'
+                        value={enteredTitle} 
+                        onChange={titleChangeHandler}></input>
                 </div>
-                <div className='new-expense_control'>
+                <div className='new-expense__control'>
                     <label>
                         Amount
                     </label>
                     <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
                 </div>
-                <div className='new-expense_control'>
+                <div className='new-expense__control'>
                     <label>
                         Date
                     </label>

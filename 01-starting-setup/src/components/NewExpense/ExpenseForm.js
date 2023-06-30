404d895c8abb2a2ea431dea9e0b2f2a2f197957a
gemnsh <shyeon5895@gmail.com>
2023-06-30 20:15:13 +0900

    edit : useState의 다른 접근법

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -3,19 +3,39 @@ import './ExpenseForm.css';
 
 function ExpenseForm(){ 
 
-    const [enteredTitle, setEnteredTitle]=useState('');
-    const [enteredAmount, setEnteredAmount]=useState('');
-    const [enteredDate, setEnteredDate]=useState('');
+    //const [enteredTitle, setEnteredTitle]=useState('');
+    //const [enteredAmount, setEnteredAmount]=useState('');
+    //const [enteredDate, setEnteredDate]=useState('');
+
+    const [ userInput, setUserInput ]=useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate:''
+    }
+    );
 
     const titleChangeHandler = (event) =>{
 
-        setEnteredTitle(event.target.value);
+        //setEnteredTitle(event.target.value);
+
+        setUserInput(
+            {
+                ...userInput,
+                enteredTitle: event.target.value,
+            }
+        )
     };
 
     const amountChangeHandler = (event) =>{
 
-        setEnteredAmount(event.target.value);
-        console.log(event.target.value);
+        //setEnteredAmount(event.target.value);
+        //console.log(event.target.value);
+        setUserInput(
+            {
+                ...userInput,
+                enteredAmount: event.target.value,
+            }
+        )
     };
 
 //    const dateChangeHandler =  (event) =>{
@@ -24,10 +44,16 @@ function ExpenseForm(){
 //        console.log(event.target.value);
 //    };
     function dateChangeHandler (event){
-        setEnteredDate(event.target.value);
-        console.log(event.target.value);
+        //setEnteredDate(event.target.value);
+        //console.log(event.target.value);
+        setUserInput(
+            {
+                ...userInput,
+                enteredDate: event.target.value,
+            }
+        )
     ;}
-    
+
     return(
         <form>
             <div className='new-expense__controls'>

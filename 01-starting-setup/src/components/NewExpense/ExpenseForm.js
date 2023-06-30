62cfb7d1122220df8b5f944b4795a923ff6994eb
gemnsh <shyeon5895@gmail.com>
2023-06-30 21:24:38 +0900

    add : 입력폼 초기화

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -69,6 +69,8 @@ function ExpenseForm(){
             date : new Date(enteredDate)
         };
         setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
         console.log(expenseData);
     };
 
@@ -88,13 +90,23 @@ function ExpenseForm(){
                     <label>
                         Amount
                     </label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
+                    <input 
+                        type='number' 
+                        min='0.01' 
+                        step='0.01' 
+                        value={enteredTitle} 
+                        onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>
                         Date
                     </label>
-                    <input type='date' min ='2000-01-01' max='2022-12-31' onChange={dateChangeHandler}></input>
+                    <input 
+                        type='date' 
+                        min ='2000-01-01' 
+                        max='2022-12-31'  
+                        value={enteredTitle} 
+                        onChange={dateChangeHandler}></input>
                 </div>
             </div>
             <div className='new-expense__actions'>

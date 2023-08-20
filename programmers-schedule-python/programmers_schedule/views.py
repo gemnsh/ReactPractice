a1d8282c4a9fe213702e9feb568ec089039bd868
gemnsh <shyeon5895@gmail.com>
2023-08-20 17:02:59 +0900

    edit : view

--- programmers-schedule-python/programmers_schedule/views.py
+++ programmers-schedule-python/programmers_schedule/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Article
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    latestArticleList=Article.objects.order_by('-articleTime')[:5]
+    output=', '.join([str(tmp.articleProblemNumber)+' / '+ tmp.articleNote for tmp in latestArticleList])
+    return HttpResponse(output)

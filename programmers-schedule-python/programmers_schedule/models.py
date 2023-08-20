b1450243c1b3c20d26562b5199fd4d41af3e80cc
gemnsh <shyeon5895@gmail.com>
2023-08-20 15:49:05 +0900

    edit : model & config

--- programmers-schedule-python/programmers_schedule/models.py
+++ programmers-schedule-python/programmers_schedule/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    articleProblemNumber=models.IntegerField()
+    articleProblemLevel=models.IntegerField()
+    articleProblemLanguage=models.CharField(max_length=8)
+    articleProblemTrial=models.IntegerField()
+    articleTime=models.DateTimeField('date published')
+    articleSolvingTime=models.IntegerField()
+    articleNote=models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.articleProblemNumber

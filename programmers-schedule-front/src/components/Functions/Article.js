8691b6db6b11d67ef65929b61900377ca06f0a5e
gemnsh <shyeon5895@gmail.com>
2023-08-26 04:46:47 +0900

    edit : 버튼 변경

--- programmers-schedule-front/src/components/Functions/Article.js
+++ programmers-schedule-front/src/components/Functions/Article.js
@@ -75,11 +75,15 @@ const Article = (props) =>{
                 </div>
             </Card>
             <div className='chartControl'>
-                <button className="chartButton" onClick={props.onPrevPageHandler}> prev </button>
+                <svg width="35px" height="35px" onClick={props.onPrevPageHandler} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.205 8.72805L12.205 3.72805C13.2041 3.10363 14.5 3.82189 14.5 5.00004V15C14.5 16.1782 13.2041 16.8965 12.205 16.272L4.205 11.272C3.265 10.6845 3.265 9.31555 4.205 8.72805Z" fill="#436EEA"/>
+                </svg>
                 <div>
                     {props.nowPage}
                 </div>
-                <button className="chartButton" onClick={props.onNextPageHandler}> next </button>
+                <svg width="35px" height="35px" onClick={props.onNextPageHandler} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.795 11.272L7.795 16.272C6.79593 16.8964 5.5 16.1782 5.5 15L5.5 5.00002C5.5 3.82186 6.79593 3.1036 7.795 3.72802L15.795 8.72802C16.735 9.31552 16.735 10.6845 15.795 11.272Z" fill="#436EEA"/>
+                </svg>
             </div>
             <ArticleList item={props.item.results} onArticleClicked={getArticleClickedHandler}/>
             <ArticleSpecific  specificData={props?props.specificData:0} articleClickChecker={props.articleClickState}/>

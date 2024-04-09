ee25bc677b2e7c99a4816f53798a98c4d1e62f6b
gemnsh <shyeon5895@gmail.com>
2024-04-09 23:44:20 +0900

    edit : 모바일 로그인 화면 수정

--- programmers-schedule-front/src/components/Functions/Login.js
+++ programmers-schedule-front/src/components/Functions/Login.js
@@ -1,4 +1,6 @@
 import React,{useState,useRef,useEffect} from "react";
+import { BrowserView, MobileView } from 'react-device-detect';
+
 import Card from "../UI/Card";
 import LoginInput from "../Input/LoginInput";
 import './Login.css';
@@ -6,8 +8,22 @@ import { ReactComponent as Circle } from "../Image/circle.svg";
 
 const Login=(props)=>{
     return(
+<div>
+    <BrowserView>
         <div className='dimmer'>
-        <Card className='login'>
+            <Card className='login'>
+                <Circle className='circle_1'/>
+                <Circle className='circle_2'/>
+                <div className='line_1'/>
+                <div className='line_2'/>
+                <div className='login_blank'/>
+                <h1>Getting Started with</h1>
+                <LoginInput setLoginModalTrue={props.setLoginModalData}/>
+            </Card>
+        </div>
+    </BrowserView>
+    <MobileView>
+        <Card className='login_mobile'>
             <Circle className='circle_1'/>
             <Circle className='circle_2'/>
             <div className='line_1'/>
@@ -16,7 +32,8 @@ const Login=(props)=>{
             <h1>Getting Started with</h1>
             <LoginInput setLoginModalTrue={props.setLoginModalData}/>
         </Card>
-        </div>
+    </MobileView>
+</div>
     )
 }
 

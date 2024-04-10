b49839a3f8784cdf2ea5f1693a6eb0aab2474077
gemnsh <shyeon5895@gmail.com>
2024-04-10 09:55:23 +0900

    edit : 디자인 수정

--- programmers-schedule-front/src/components/Input/NewData.js
+++ programmers-schedule-front/src/components/Input/NewData.js
@@ -122,31 +122,31 @@ const NewData =(props) =>{
             <MobileView>
                 <Card className='new-data_mobile' bgColor={props.sendThemeArray.color_01}>
                     <form onSubmit={submitHandler}>
-                        <div className='new-data__control'>
+                        <div className='new-data__control_mobile'>
                             <label style={{color:props.sendThemeArray.color_02}}>
                                 문제번호
                             </label>
                             <input id ='pNumber' name="problemNumber" style={{border:props.sendThemeArray.color_05}} onChange ={(e) => setResponseBody({...responseBody, problemNumber: e.target.value})}/>
                         </div>
-                        <div className='new-data__control'>
+                        <div className='new-data__control_mobile'>
                             <label style={{color:props.sendThemeArray.color_02}}>
                                 레벨
                             </label>
                             <MultiButton buttonArr={levelArray} onSetButtonResponse={setButtonResponse1}widthButton={50} sendThemeArray={props.sendThemeArray}/>
                         </div>
-                        <div className='new-data__control'>
+                        <div className='new-data__control_mobile'>
                             <label style={{color:props.sendThemeArray.color_02}}>
                                 언어
                             </label>
                             <MultiButton buttonArr={langArray} onSetButtonResponse={setButtonResponse2}  widthButton={80} sendThemeArray={props.sendThemeArray}/>
                         </div>
-                        <div className='new-data__control'>
+                        <div className='new-data__control_mobile'>
                             <label style={{color:props.sendThemeArray.color_02}}>
                                 시도횟수
                             </label>
                             <input id ='pTrial' name="trial" style={{border:props.sendThemeArray.color_05}} onChange ={(e) => setResponseBody({...responseBody, trial: e.target.value})}/>
                         </div>
-                        <div className='new-data__control'>
+                        <div className='new-data__control_mobile'>
                             <label style={{color:props.sendThemeArray.color_02}}>
                                 시간
                             </label>
@@ -156,7 +156,7 @@ const NewData =(props) =>{
                             <button id='submit_button' type='submit' disabled={!props.buttonStateDatas} style={props.buttonStateDatas?{border:props.sendThemeArray.color_05,backgroundColor:props.sendThemeArray.color_05}:{border:props.sendThemeArray.color_08,backgroundColor:props.sendThemeArray.color_08,color:props.sendThemeArray.color_08}}>입력</button>
                         </div>
 
-                        <div className='new-data__control2'>
+                        <div className='new-data__control2_mobile'>
 
                             <textarea 
                             id='pNote' 

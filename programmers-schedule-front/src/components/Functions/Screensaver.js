cdb142d455be499c100802214a24d7649f7c86dc
gemnsh <shyeon5895@gmail.com>
2024-04-11 15:08:02 +0900

    edit : 화면보호기 디자인 수정

--- programmers-schedule-front/src/components/Functions/Screensaver.js
+++ programmers-schedule-front/src/components/Functions/Screensaver.js
@@ -8,13 +8,21 @@ const Screensaver = (props) =>{
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+    const formatTime = (tmp) => {
+        const getSeconds = `0${(tmp % 60)}`.slice(-2);
+        const minutes = `${Math.floor(tmp / 60)}`;
+        const getMinutes = `0${minutes % 60}`.slice(-2);
+        const getHours = `0${Math.floor(tmp / 3600)}`.slice(-2);
+        return(`${getHours}:${getMinutes}:${getSeconds}`);
+    }
+
     return(
         <div className='screen_mobile'>
             <div className='screen_svg'>
             <Eclipse />
             </div>
             <div className='screen_text'>
-                {props.stopWatchTime}
+                {formatTime(props.stopWatchTime)}
             </div>
         </div>
     );

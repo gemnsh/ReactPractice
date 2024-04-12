f6813f080e3026579e98ddef3150afe7c1322bde
gemnsh <shyeon5895@gmail.com>
2024-04-12 14:12:06 +0900

    fix : 화면보호기 기능

--- programmers-schedule-front/src/App.js
+++ programmers-schedule-front/src/App.js
@@ -286,7 +286,7 @@ const App =() =>{
   const onTouchEnd=(e)=>{
     setLastTouch(stopWatchTimeData);
     setScreensaverState(false)
-    e.stopImmediatePropagation();
+    return false;
   }
 
   useEffect(()=>{
